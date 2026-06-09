4a04f323159cb40aba2f2d390fff72bf75ca8db3
Loic Andrianarivony <loic.andrianarivony31@gmail.com>
2026-06-09 20:17:23 +0200

    feat : add agency CRUD endpoints

--- js/app.js
+++ js/app.js
@@ -14,6 +14,9 @@ app.use('/properties', propertyRoutes);
 const appointmentRoutes = require('./routes/appointmentRoutes');
 app.use('/appointments', appointmentRoutes);
 
+const agencyRoutes = require('./routes/agencyRoutes');
+app.use('/agencies', agencyRoutes);
+
 app.listen(3000, () => {
   console.log('Serveur lancé sur le port 3000');
 });
