1420abe2becf70c406ec4c42b15f717b5a6a9a79
Sinigaglia Lorenzo <lorenzo.sinigaglia@ynov.com>
2026-06-13 10:14:18 +0200

    feat(security): add input validation and XSS escaping on agency routes

--- js/routes/agencyRoutes.js
+++ js/routes/agencyRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const roleMiddleware = require('../middleware/roleMiddleware');
+const validateRequest = require('../middleware/validateRequest');
+const { createAgencyValidator, updateAgencyValidator, agencyIdParamValidator } = require('../middleware/validators/agencyValidators');
 const {
   getAllAgencies,
   getAgencyById,
@@ -11,9 +13,9 @@ const {
 } = require('../controllers/agencyController');
 
 router.get('/',       getAllAgencies);
-router.get('/:id',    getAgencyById);
-router.post('/',      authMiddleware, roleMiddleware('admin'), createAgency);
-router.put('/:id',    authMiddleware, roleMiddleware('admin'), updateAgency);
-router.delete('/:id', authMiddleware, roleMiddleware('admin'), deleteAgency);
+router.get('/:id',    agencyIdParamValidator, validateRequest, getAgencyById);
+router.post('/',      authMiddleware, roleMiddleware('admin'), createAgencyValidator, validateRequest, createAgency);
+router.put('/:id',    authMiddleware, roleMiddleware('admin'), agencyIdParamValidator, updateAgencyValidator, validateRequest, updateAgency);
+router.delete('/:id', authMiddleware, roleMiddleware('admin'), agencyIdParamValidator, validateRequest, deleteAgency);
 
 module.exports = router;
